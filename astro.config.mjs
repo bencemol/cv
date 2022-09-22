f9848c68882a4f2b8c15e117b8db1ee5678605e7
Bence Molnár <bence.molnar@snapsoft.hu>
2022-09-22 16:46:14 +0200

    fix url base

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,6 @@ import { defineConfig } from "astro/config";
 // https://docs.astro.build/en/reference/configuration-reference/
 export default defineConfig({
   integrations: [],
-  site: "https://bencemol.github.io/cv",
+  site: "https://bencemol.github.io",
+  base: "/cv",
 });
