c0cd2ce8d1132b0e6d847ee1049018784573fc44
Bence Molnár <bence.molnar@snapsoft.hu>
2022-09-22 16:19:24 +0200

    bump astro version & update content & hover animations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,7 @@
-// Full Astro Configuration API Documentation:
-// https://docs.astro.build/reference/configuration-reference
+import { defineConfig } from "astro/config";
 
-// @type-check enabled!
-// VSCode and other TypeScript-enabled text editors will provide auto-completion,
-// helpful tooltips, and warnings if your exported object is invalid.
-// You can disable this by removing "@ts-check" and `@type` comments below.
-
-// @ts-check
-export default /** @type {import('astro').AstroUserConfig} */ ({
-	// Comment out "renderers: []" to enable Astro's default component support.
-	renderers: [],
-	buildOptions: {
-		site: "https://bencemol.github.io/cv"
-	}
+// https://docs.astro.build/en/reference/configuration-reference/
+export default defineConfig({
+  integrations: [],
+  site: "https://bencemol.github.io/cv",
 });
