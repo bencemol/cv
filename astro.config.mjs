f3b1704737a74920c522f0254b0fefe49e3be3a7
Bence Molnár <bence.molnar@snapsoft.hu>
2023-02-27 19:33:56 +0100

    remove purgecss

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,7 @@
 import { defineConfig } from "astro/config";
 
-// https://docs.astro.build/en/reference/configuration-reference/
-import purgecss from "astro-purgecss";
-
-// https://astro.build/config
 export default defineConfig({
-  integrations: [purgecss()],
+  integrations: [],
   site: "https://bencemol.github.io",
   // base: "/cv",
 });
